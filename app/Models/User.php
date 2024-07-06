5ff02ffd41e1dce04efac50f140a8ae6306faa11
farhanrizkyr <farhanrizkyramadhan99@gmail.com>
2024-07-06 17:01:53 +0700

    (Setting(Set up Template User)

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function avatar()
+   {
+      if (!$this->avatar) {
+        return asset('Avatar/avatar.png');
+      }
+      return asset('Avatar/'.$this->avatar);
+   }
 }
