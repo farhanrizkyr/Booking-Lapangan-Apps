1d6b086034121b8b1c4f09b516d98a40e0286e6a
farhanrizkyr <farhanrizkyramadhan99@gmail.com>
2024-07-06 17:01:53 +0700

    (Setting(Set up Template User)

--- resources/views/User/dashboard.blade.php
+++ resources/views/User/dashboard.blade.php
@@ -1,14 +1,19 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dahboard demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Dahboard demo</h1>
-    <a href="{{route('logout')}}">Logout</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+@extends('Components.Layout')
+@section('title','Dashboard')
+@section('main')
+    
+<div class="pagetitle">
+  <h1>Halaman Dashboard</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="/apps-user/dashboard">Dashboard</a></li>
+      <li class="breadcrumb-item">Halaman Dashboard</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section">
+ 
+
+</section>
+@endsection
